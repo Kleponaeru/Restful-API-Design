b1d0c23ac11d4e12c66b824ac39df1f012a1026a
Kleponaeru <kelvinlie08@gmail.com>
2023-05-08 13:44:17 +0700

    Re commit

--- BookStoreApi/Controllers/BooksController.cs
+++ BookStoreApi/Controllers/BooksController.cs
@@ -1,75 +1,88 @@
 using BookStoreApi.Models;
 using BookStoreApi.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json;
+using System.Net;
+using System.Net.Http;
 
+namespace BookStoreApi.Controllers;
 
-namespace BookStoreApi.Controllers
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class BooksController : ControllerBase
+    private readonly BooksService _booksService;
+
+    public BooksController(BooksService booksService) =>
+        _booksService = booksService;
+
+    [HttpGet]
+    public async Task<List<Book>> Get() =>
+        await _booksService.GetAsync();
+
+    [HttpGet("{id:length(24)}")]
+    public async Task<ActionResult<Book>> Get(string id)
     {
-        private readonly BooksService _booksService;
+        var book = await _booksService.GetAsync(id);
 
-        public BooksController(BooksService booksService) =>
-            _booksService = booksService;
-
-        [HttpGet]
-        public async Task<List<Book>> Get() =>
-            await _booksService.GetAsync();
-
-        [HttpGet("{id:length(24)}")]
-        public async Task<ActionResult<Book>> Get(string id)
+        if (book is null)
         {
-            var book = await _booksService.GetAsync(id);
-
-            if (book is null)
-            {
-                return NotFound();
-            }
-
-            return book;
+            return NotFound();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Post(Book newBook)
+        return book;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Book newBook)
+    {
+        try
         {
+            if (newBook == null)
+            {
+                return BadRequest();
+            }
+
             await _booksService.CreateAsync(newBook);
 
             return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
         }
 
-        [HttpPut("{id:length(24)}")]
-        public async Task<IActionResult> Update(string id, Book updatedBook)
+        catch (Exception)
         {
-            var book = await _booksService.GetAsync(id);
-
-            if (book is null)
-            {
-                return NotFound();
-            }
-
-            updatedBook.Id = book.Id;
-
-            await _booksService.UpdateAsync(id, updatedBook);
-
-            return NoContent();
-        }
-
-        [HttpDelete("{id:length(24)}")]
-        public async Task<IActionResult> Delete(string id)
-        {
-            var book = await _booksService.GetAsync(id);
-
-            if (book is null)
-            {
-                return NotFound();
-            }
-
-            await _booksService.RemoveAsync(id);
-
-            return NoContent();
+            return StatusCode(StatusCodes.Status500InternalServerError,
+            "Error retrieving data from the database");
         }
     }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, Book updatedBook)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        updatedBook.Id = book.Id;
+
+        await _booksService.UpdateAsync(id, updatedBook);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        await _booksService.RemoveAsync(id);
+
+        return NoContent();
+    }
 }
--- BookStoreApi/Models/Book.cs
+++ BookStoreApi/Models/Book.cs
@@ -2,6 +2,7 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
+
 namespace BookStoreApi.Models;
 
 public class Book
@@ -11,18 +12,11 @@ public class Book
     public string? Id { get; set; }
 
     [BsonElement("Name")]
-    [JsonPropertyName("Name")]
-    [Required]
-    [StringLength(100, MinimumLength = 2)]
     public string BookName { get; set; } = null!;
+    [Required]
+    public decimal? Price { get; set; } = null!;
 
-    [Required]
-    [Range(0, 999)]
-    public decimal Price { get; set; }
-    [Required]
-    [StringLength(100, MinimumLength = 2)]
-    
     public string Category { get; set; } = null!;
-
-    public string Author { get; set; } = null!;
+    
+    public string Author { get; set;} = null!;
 }
