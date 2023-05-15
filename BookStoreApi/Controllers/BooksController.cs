c16d363a4c14ba038961d4605e8d074c4d8943c5
Kleponaeru <kelvinlie08@gmail.com>
2023-05-15 13:27:28 +0700

    Pertemuan 10: Swashbuckle

--- BookStoreApi/Controllers/BooksController.cs
+++ BookStoreApi/Controllers/BooksController.cs
@@ -8,18 +8,66 @@ namespace BookStoreApi.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[Produces("application/json")]
 public class BooksController : ControllerBase
 {
     private readonly BooksService _booksService;
 
     public BooksController(BooksService booksService) =>
         _booksService = booksService;
-
+    /// <summary>
+    /// Get all BooksItem data.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <response code="400">If the item is null</response>
+    /// <response code="401">If the user is not authorized to access the resource</response>
+    /// <response code="404">If the resource could not be found</response>
+    /// <response code="500">If there was an internal server error</response>
+    ///<remarks>
+    /// Sample request:
+    ///
+    ///     GET /BookStore
+    ///     {
+    ///        "id": 1,
+    ///        "name": "Item #1",
+    ///        "isComplete": true
+    ///     }
+    ///
+    /// </remarks>
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<List<Book>> Get() =>
         await _booksService.GetAsync();
 
+    /// <summary>
+    /// Get specific BooksItem.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <response code="400">If the item is null</response>
+    /// <response code="401">If the user is not authorized to access the resource</response>
+    /// <response code="404">If the resource could not be found</response>
+    /// <response code="500">If there was an internal server error</response>
+     /// <remarks>
+    /// Sample request:
+    ///
+    ///     GET /BookStore
+    ///     {
+    ///        "id": "string
+    ///     }
+    ///
+    /// </remarks>
     [HttpGet("{id:length(24)}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<ActionResult<Book>> Get(string id)
     {
         var book = await _booksService.GetAsync(id);
@@ -31,8 +79,37 @@ public class BooksController : ControllerBase
 
         return book;
     }
-
+    /// <summary>
+    /// Creates a BooksItem.
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns>A newly created BooksItem</returns>
+    /// <remarks>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     POST /BookStore
+    ///     {
+    ///        "id": "string,
+    ///        "BookName": "Item #1",
+    ///        "Price": 0,
+    ///        "Category": "Cateory #1",
+    ///        "Author" : "Author #1";
+    ///     }
+    ///
+    /// </remarks>
+    /// </remarks>
+    /// <response code="201">Returns the newly created item</response>
+    /// <response code="400">If the item is null</response>
+    /// <response code="401">If the user is not authorized to access the resource</response>
+    /// <response code="404">If the resource could not be found</response>
+    /// <response code="500">If there was an internal server error</response>
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> Post(Book newBook)
     {
         try
@@ -54,7 +131,33 @@ public class BooksController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Update specific BooksItem.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <response code="400">If the item is null</response>
+    /// <response code="401">If the user is not authorized to access the resource</response>
+    /// <response code="404">If the resource could not be found</response>
+    /// <response code="500">If there was an internal server error</response>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     PUT /BookStore
+    ///     {
+    ///        "id": "string,
+    ///        "BookName": "Item #1",
+    ///        "Price": 0,
+    ///        "Category": "Cateory #1",
+    ///        "Author" : "Author #1";
+    ///     }
+    ///
+    /// </remarks>
     [HttpPut("{id:length(24)}")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> Update(string id, Book updatedBook)
     {
         var book = await _booksService.GetAsync(id);
@@ -70,8 +173,33 @@ public class BooksController : ControllerBase
 
         return NoContent();
     }
-
+    /// <summary>
+    /// Deletes a specific BooksItem.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <response code="400">If the item is null</response>
+    /// <response code="401">If the user is not authorized to access the resource</response>
+    /// <response code="404">If the resource could not be found</response>
+    /// <response code="500">If there was an internal server error</response>
+     /// <remarks>
+    /// Sample request:
+    ///
+    ///     DELETE /BookStore
+    ///     {
+    ///        "id": "string,
+    ///        "BookName": "Item #1",
+    ///        "Price": 0,
+    ///        "Category": "Cateory #1",
+    ///        "Author" : "Author #1";
+    ///     }
+    ///
+    /// </remarks>
     [HttpDelete("{id:length(24)}")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> Delete(string id)
     {
         var book = await _booksService.GetAsync(id);
@@ -84,5 +212,5 @@ public class BooksController : ControllerBase
         await _booksService.RemoveAsync(id);
 
         return NoContent();
-    }
+    }
 }
--- BookStoreApi/Program.cs
+++ BookStoreApi/Program.cs
@@ -1,5 +1,7 @@
 using BookStoreApi.Models;
+using Microsoft.OpenApi.Models;
 using BookStoreApi.Services;
+using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.Configure<BookStoreDatabaseSettings>(
@@ -10,7 +12,29 @@ builder.Services.AddControllers()
         options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "1.0",
+        Title = "Book Store API",
+        Description = "An ASP.NET Core Web API for managing BookStore items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+     // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
